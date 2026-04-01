84c7a123abe9c006406beccaa7e8e929cd62feab
Katsudon27 <katsudon1127@gmail.com>
2026-04-01 23:08:45 +1100

    Add function to play single round

--- javascript.js
+++ javascript.js
@@ -11,8 +11,50 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    return(prompt("Enter R for Rock, P for Paper, or S for Scissors: "));
+    return(prompt("Enter Rock, Paper, or Scissors: "));
 }
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+
+    if ((humanChoice == "rock" && computerChoice == "scissors") |(humanChoice == "paper" && computerChoice == "rock") | (humanChoice == "scissors" && computerChoice == "paper")){
+        switch (humanChoice) {
+            case "rock":
+                console.log("You win! Rock beats Scissors.");
+                break;
+            
+            case "scissors":
+                console.log("You win! Scissors beats Paper.");
+                break;
+            
+            case "paper":
+                console.log("You win! Paper beats Rock.");
+                break;
+        }
+        humanScore += 1;
+    }else{
+        switch (computerChoice) {
+            case "rock":
+                console.log("You lose! Rock beats Scissors.");
+                break;
+            
+            case "scissors":
+                console.log("You lose! Scissors beats Paper.");
+                break;
+            
+            case "paper":
+                console.log("You lose! Paper beats Rock.");
+                break;
+        }
+        computerScore += 1;
+    }
+}
+
 
 let humanScore = 0;
 let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
